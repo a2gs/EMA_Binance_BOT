4659640beed9475072ea2e8450abea13bb50b8a3
Andre Augusto Giannotti Scota <andre.scota@gmail.com>
2019-09-23 22:42:54 -0300

    kill bot function

--- botEnv.sh
+++ botEnv.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-export BINANCE_APIKEY=''
-export BINANCE_SEKKEY=''
+export BINANCE_APIKEY='372YzaY3nTItd1Z8SE8TZH8vFkdnnJuxAiRCM3KwZq3lmJewL4ta2LO5VgsaSI0Y'
+export BINANCE_SEKKEY='XTaSTNiOEbvmCmp5zyFChxkJLQYdb85sRBf8cgP5qCn8JYiEXRpYVsX9Hjt1MRsW'
 
-alias killBot=''
+function kbot(){
+	kill `head -n 1 $1_pid.text`
+}
