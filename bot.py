c8a38ca3bc875e4c7e20bd9270285c3f102c0223
Andre Augusto Giannotti Scota <andre.scota@gmail.com>
2019-11-26 19:28:05 -0300

    Notify class

--- bot.py
+++ bot.py
@@ -14,6 +14,7 @@ import signal
 import logging
 
 import ema
+import notify
 
 from cfg import botCfg
 from cfg import klineAPIIntervals
---
+++ notify.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Andre Augusto Giannotti Scota
+# andre.scota@gmail.com
+# MIT license
+
+import tweepy
+
+class notify:
+	def __init__(type):
+		if type.lower() == 'twitter':
+			pass
+		elif type.lower() == 'log':
+			pass
+		else:
+			pass
