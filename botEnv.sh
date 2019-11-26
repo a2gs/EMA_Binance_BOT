6d212e4e4fe45890795378a502ae8e0581194d33
Andre Augusto Giannotti Scota <andre.scota@gmail.com>
2019-11-26 19:11:40 -0300

    Twitter environment variables

--- botEnv.sh
+++ botEnv.sh
@@ -1,7 +1,23 @@
 #!/usr/bin/env bash
 
+# ------------------------------------------------------------
+# Binance Token
 export BINANCE_APIKEY=''
 export BINANCE_SEKKEY=''
+
+# ------------------------------------------------------------
+# Twitter token
+
+# Consumer API keys
+export TWITTER_APIKEY=''
+export TWITTER_APISEKKEY=''
+
+# Access token & access token secret
+
+export TWITTER_ACCSSTKN=''
+export TWITTER_ACCSSSEKTKN=''
+
+# ------------------------------------------------------------
 
 function kbot(){
 	kill `head -n 1 $1_pid.text`
